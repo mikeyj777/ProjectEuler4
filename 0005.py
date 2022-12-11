ae706c24f218e229350a9aa3b2e5f7065c568079
Michael James <mikeyj777@gmail.com>
2022-12-11 00:10:09 -0500

    started 5.  fixed prime factorization util

---
+++ 0005.py
@@ -0,0 +1,10 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# get divisors of max number
+# if divisors are in 
+
+import util_4
+
+print(util_4.prime_factors(20))
--- util_4.py
+++ util_4.py
@@ -30,31 +30,22 @@ def prime_factors(n):
         return []
     l = int(math.sqrt(n))
 
-    arr = np.arange(3,l+1,2)
+    arr = np.arange(3,n,2)
+
+    arr = np.insert(arr, 0, 2)
 
     primes = []
 
     val = n
 
-    
-
-    if n % 2 == 0:
-        primes.append(2)
-        val /= n
-    
-    if n % 3 == 0:
-        primes.append(3)
-        val /= n
-
-    arr = arr[arr % 3 != 0]
-
-    num = 6
     while val > 1 and len(arr) > 0:
         if val % arr[0] == 0:
             val /= arr[0]
             primes.append(arr[0])
-        arr = arr[arr % arr[0] != 0]
+        else:
+            arr = arr[arr % arr[0] != 0]
 
 
     return primes
 
+print(prime_factors(20))
